ad37615aaddf4c7b0b8ecf9f488a8cb5749ce8e7
AK-3d <abdulkareemHa.ye@gmail.com>
2023-06-11 03:32:08 +0300

    Scafolding Connection class

---
+++ include/Connection.h
@@ -0,0 +1,57 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebSrv.h>
+#include <WiFiUdp.h>
+#include <DNSServer.h>
+
+
+#define UDP_ADRS "239.1.2.3"
+#define UDP_PORT 54321
+
+class Connection{
+public:
+    IPAddress apIP;
+    DNSServer dnsServer;
+    WiFiUDP udp;
+    IPAddress ip;
+
+    AsyncWebServer server;
+    AsyncWebSocket websocket;
+
+    
+    const char* ssid = "YourAPSSID";
+    const char* password = "YourAPPassword";
+
+    // const char* ssid = "Ak";
+    // const char* password = "@AK@-12#";
+
+    // const char* ssid = "tripleAAA";
+    // const char* password = "12345678900";
+
+    // const char* ssid = "root";
+    // const char* password = "maxmax123";
+
+
+    String message = "Initial message";
+
+    unsigned long interval;
+    long num;
+    String ipaddress;
+    
+    Connection();
+    void setup();
+    void update();
+
+    void setupWIFI();
+    void setupServer();
+    void setupWebsocket();
+    void setupDNSserver();//this can be used to access the server with anyname.local address 
+
+    void broadcastIP();
+    void broadcastMsg(String msg);
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,42 @@
 #include <FlowSensor.h>
-#include<TdsSensor.h>
+#include <TdsSensor.h>
+#include <Connection.h>
+
 #define calibrationFactorValue 80.0
 
-FlowSensor prioriFlow(27, calibrationFactorValue);
-FlowSensor subFlow(14, calibrationFactorValue);
-TdsSensor tds(13,24);
-unsigned long currentMillis = millis();
-unsigned long previousMillis =currentMillis;
-void increasePulseCountPrioriFlow()
-{
+Connection connection;
 
-    prioriFlow.increasePulseCount();
+// FlowSensor prioriFlow(27, calibrationFactorValue);
+// FlowSensor subFlow(14, calibrationFactorValue);
+// TdsSensor tds(13,24);
+
+// unsigned long currentMillis = millis();
+// unsigned long previousMillis =currentMillis;
+// void increasePulseCountPrioriFlow()
+// {
+
+//     prioriFlow.increasePulseCount();
+// }
+// void increasePulseCountSubFlow()
+// {
+
+//     subFlow.increasePulseCount();
+// }
+
+void updateMsg(){
+    connection.broadcastIP();
+    connection.broadcastMsg(String(random(1,100)));
 }
-void increasePulseCountSubFlow()
-{
-
-    subFlow.increasePulseCount();
-}
-
 void setup()
-{   Serial.begin(112500);;
-    prioriFlow.begin();
-    subFlow.begin();
-    attachInterrupt(digitalPinToInterrupt(prioriFlow.pin), increasePulseCountPrioriFlow, RISING);
-    attachInterrupt(digitalPinToInterrupt(subFlow.pin), increasePulseCountSubFlow, FALLING);
-    tds.begin();
+{   
+    Serial.begin(112500);;
+
+    connection.setup();
+    // prioriFlow.begin();
+    // subFlow.begin();
+    // attachInterrupt(digitalPinToInterrupt(prioriFlow.pin), increasePulseCountPrioriFlow, RISING);
+    // attachInterrupt(digitalPinToInterrupt(subFlow.pin), increasePulseCountSubFlow, FALLING);
+    // tds.begin();
     
 
 }
@@ -42,16 +54,23 @@ void loop()
     // Serial.println(subFlow.literPassed()); 
     // delay(1000);
 
-Serial.print("TDS Value:");
-Serial.print(tds.finalMeasure(),0);
-Serial.println("ppm");
-Serial.print("Temp:");
-Serial.print(tds.temperature,0);
-Serial.println("C");
-currentMillis = millis();
-if(currentMillis >(previousMillis+15000)){
-    tds.temperature =25;
-    previousMillis =currentMillis;
-}
+    // Serial.print("TDS Value:");
+    // Serial.print(tds.finalMeasure(),0);
+    // Serial.println("ppm");
+    // Serial.print("Temp:");
+    // Serial.print(tds.temperature,0);
+    // Serial.println("C");
+    // currentMillis = millis();
+    // if(currentMillis >(previousMillis+15000)){
+    //     tds.temperature =25;
+    //     previousMillis =currentMillis;
+    // }
 
+    connection.update();    
+    if((millis() - connection.interval) > 2000)
+    {   
+        updateMsg();
+
+        connection.interval = millis();
+    }
 }
