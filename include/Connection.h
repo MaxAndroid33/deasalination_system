e31a6cb5c09ad4aaac4c01928147eb0ddd714fb2
Yousif Alqiez <y.alqiez@xairinformatics.com>
2023-07-10 05:15:35 +0300

    adding : 1. add the ssid & password to the constructor . 2. add the inputPosition for the servo control using webServer http . 3. add the innerPump control for on and off the pumps using webServer http .

--- include/Connection.h
+++ include/Connection.h
@@ -7,11 +7,11 @@
 #include <WiFiUdp.h>
 #include <DNSServer.h>
 
-
 #define UDP_ADRS "239.1.2.3"
 #define UDP_PORT 54321
 
-class Connection{
+class Connection
+{
 public:
     IPAddress apIP;
     DNSServer dnsServer;
@@ -21,9 +21,8 @@ public:
     AsyncWebServer server;
     AsyncWebSocket websocket;
 
-    
-    const char* ssid = "YourAPSSID";
-    const char* password = "12345678";
+    const char *ssid;
+    const char *password;
 
     // const char* ssid = "Ak";
     // const char* password = "@AK@-12#";
@@ -34,21 +33,22 @@ public:
     // const char* ssid = "root";
     // const char* password = "maxmax123";
 
-
     String message = "Initial message";
-    int inputPostion=170;
+    int inputPosition = 170;
+    int pumpState = 1;
     unsigned long interval;
     long num;
     String ipaddress;
-    
+
     Connection();
+    Connection(const char *ssid, const char *password);
     void setup();
     void update();
 
     void setupWIFI();
     void setupServer();
     void setupWebsocket();
-    void setupDNSserver();//this can be used to access the server with anyname.local address 
+    void setupDNSserver(); // this can be used to access the server with anyname.local address
 
     void broadcastIP();
     void broadcastMsg(String msg);
