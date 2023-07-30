1d2ec25e57a9d3195e4c335e34f85a6d4019d2a9
yunus700466271 <103322678+yunus700466271@users.noreply.github.com>
2023-07-30 04:06:45 +0300

    fix : fixing the duration time for pulse return timeout

--- src/TankLevel.cpp
+++ src/TankLevel.cpp
@@ -37,12 +37,8 @@ long TankLevel::monitor()
   digitalWrite(trigPin, LOW);
   
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH,200UL);
   
- 
-  
- 
-
     return duration;
 }
 
