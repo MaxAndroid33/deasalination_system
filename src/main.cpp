6c16223e3a0f12274e040cc786ed2dedef750b22
maxandroid33 <yuosifmax309@gmail.com>
2023-08-25 14:55:23 +0300

    fix some error

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,9 @@
 
 // const char *ssid = "tripleAAA";
 // const char *password = "12345678900";
-const char *ssid = "hello";
-const char *password = "helloWorld";
+const char *ssid = "import4G";
+const char *password = "max*!@.77013*!@";
+
 
 Connection connection(ssid, password);
 FlowSensor permeate_flow(FLOWMETER_PERMEATE_PIN, calibrationFactorValue);       // Permeate (purified water)
@@ -46,16 +47,13 @@ void increasePulseCountSubFlow();
 void handleMessage(void *arg, uint8_t *data, size_t len);
 void updateMsg();
 void updateFastMsg();
-void simulate();
 
-int tankLevel1 = 100;
-int tankLevel2 = 100;
 String data;
 
 void setup()
 {
     Serial.begin(115200);
-    connection.setup(handleMessage);
+    connection.setup(handleMessage,false);
 
     permeate_flow.begin();
     concentrate_flow.begin();
@@ -72,11 +70,11 @@ void setup()
     tankDrink.setMaxLevelTank(28);
     tankDrink.begin();
 
-    out.runPump("plantpump", boolean(0));
-    out.runPump("drinkpump", boolean(0));
-    out.runPump("innerpump", boolean(0));
-    out.runPump("plant", boolean(0));
-    out.runPump("drink", boolean(0));
+    // out.runPump("plantpump", boolean(1));
+    // out.runPump("drinkpump", boolean(1));
+    // out.runPump("innerpump", boolean(1));
+    // out.runPump("plant", boolean(1));
+    // out.runPump("drink", boolean(1));
 }
 
 void loop()
@@ -88,12 +86,12 @@ void loop()
     currentSensor.readCurrent();
     tankPlant.monitor();
     tankDrink.monitor();
-    tds.measure();
+    tds.measure();  
     connection.update();
-    simulate();
+
     updateFastMsg();
 
-    if ((millis() - connection.interval) > 2000)
+    if ((millis() - connection.interval) > 500)
     {
         updateMsg();
         connection.interval = millis();
@@ -157,11 +155,11 @@ void handleMessage(void *arg, uint8_t *data, size_t len)
             tankPlant.setMaxLevelTank(value);
             tankDrink.setMaxLevelTank(value);
         }
-        else if (strcmp(key, "plant") == 0)
+        else if (strcmp(key, "plantvalve") == 0)
         {
             out.openValve("plant", boolean(int(value)));
         }
-        else if (strcmp(key, "drink") == 0)
+        else if (strcmp(key, "drinkvalve") == 0)
         {
             out.openValve("drink", boolean(int(value)));
         }
@@ -187,10 +185,10 @@ void updateMsg()
 
     String tankData = String(LIVETANK) + "=" +
                       String(PORT) + ":1" + "," +
-                      String(LEVEL) + ":" + String(tankLevel1) + "," +
+                      String(LEVEL) + ":" + String(tankPlant.tankLevelPresent()) + "," +
                       String(ISFILLING) + ":" + String(tankPlant.status()) + "|" +
                       String(LIVETANK) + "=" + String(PORT) + ":2" + "," +
-                      String(LEVEL) + ":" + String(tankLevel2) + "," +
+                      String(LEVEL) + ":" + String(tankDrink.tankLevelPresent()) + "," +
                       String(ISFILLING) + ":" + String(tankDrink.status()) + "|";
 
     String pumpAndValveData = String(PUMPSANDVALVES) + "=" +
@@ -200,71 +198,55 @@ void updateMsg()
                               String(PLANTVALVE) + ":" + String(out.getBoolState(PLANTVALVE)) + "," +
                               String(DRINKVALVE) + ":" + String(out.getBoolState(DRINKVALVE)) + "|";
 
-    String powerData = String(POWER) + "=" + 
-                       String(VOLTAGEIN)+ ":" + String(voltageSensor.voltage_measured()) + "," +
+    String powerData = String(POWER) + "=" +
+                       String(VOLTAGEIN) + ":" + String(voltageSensor.voltage_measured()) + "," +
                        String(CURRENTIN) + ":0" + "," +
                        String(CURRENTOUT) + ":" + String(currentSensor.readCurrent()) + "," +
                        String(BATTERYLEVEL) + ":" + String(voltageSensor.battery_level()) + "," +
                        String(ISBATTERY) + ":1" + "," +
                        String(DURATION) + ":1" + "|";
-  
 
     connection.broadcastMsg(tankData + pumpAndValveData + powerData);
 }
-void updateFastMsg(){
+void updateFastMsg()
+{
     static int fastInterval = millis();
 
-    
-    if((millis() - fastInterval) >  100) {
-        
+    if ((millis() - fastInterval) > 100)
+    {
+
         data = "";
-        if(out.getBoolState(MAINPUMP) || out.getBoolState(DRINKPUMP)){
+        if (out.getBoolState(MAINPUMP) || out.getBoolState(DRINKPUMP))
+        {
             data += String(LIVETANK) + "=" +
-                        String(PORT) + ":1," +
-                        String(LEVEL) + ":" + String(tankLevel1) + "," +
-                        String(ISFILLING) + ":" + String(tankPlant.status()) + "|";
-        }
-        
-        if(out.getBoolState(MAINPUMP) || out.getBoolState(PLANTPUMP)){
-            data += String(LIVETANK) + "=" + 
-                        String(PORT) + ":2," +
-                        String(LEVEL) + ":" + String(tankLevel2) + "," +
-                        String(ISFILLING) + ":" + String(tankDrink.status()) + "|";
-        }
-        
-        if(out.getBoolState(MAINPUMP)){
-            data += String(PRODUCTION) + "=" +
-                            String(TDS) + ":" + String((int(millis() / 500) + 10000) % 10000) + "," +
-                            // String(TDS) + ":" + String(tds.measure()) + "," +
-                            //String(TEMPERATURE) + ":" + String(tds.temperatureMeasure()) + "," +
-                            String(TEMPERATURE) + ":" + String(0) + "," +
-                            String(PREFLOW) + ":" + String(permeate_flow.flowRate()) + "," +
-                            String(CONFLOW) + ":" + String(concentrate_flow.flowRate()) + "|";
+                    String(PORT) + ":1," +
+                    String(LEVEL) + ":" + String(tankPlant.tankLevelPresent()) + "," +
+                    String(ISFILLING) + ":" + String(tankPlant.status()) + "|";
         }
 
-        if(data.length())
+        if (out.getBoolState(MAINPUMP) || out.getBoolState(PLANTPUMP))
+        {
+            data += String(LIVETANK) + "=" +
+                    String(PORT) + ":2," +
+                    String(LEVEL) + ":" + String(tankDrink.tankLevelPresent()) + "," +
+                    String(ISFILLING) + ":" + String(tankDrink.status()) + "|";
+        }
+
+        if (out.getBoolState(MAINPUMP))
+        {
+            data += String(PRODUCTION) + "=" +
+                    // String(TDS) + ":" + String((int(millis() / 500) + 10000) % 10000) + "," +
+                    String(TDS) + ":" + String(tds.measure()) + "," +
+                    String(TEMPERATURE) + ":" + String(25.7) + "," +
+                    // String(TEMPERATURE) + ":" + String(0) + "," +
+                    String(PREFLOW) + ":" + String(permeate_flow.flowRate()) + "," +
+                    String(CONFLOW) + ":" + String(concentrate_flow.flowRate()) + "|";
+        }
+
+        if (data.length())
             connection.broadcastMsg(data);
 
         fastInterval = millis();
         Serial.println(fastInterval);
-    }   
-}
-
-void simulate(){
-    static int inter = 0;
-    if((millis() - inter) > 500){
-        if(out.getBoolState(DRINKPUMP) == 1){
-            tankLevel1 -=0.001;
-        }
-        else{
-            tankLevel1 = 100;
-        }
-        if(out.getBoolState(PLANTPUMP) == 1){
-            tankLevel2 -=0.001;
-        }
-        else{
-            tankLevel2 = 100;
-        }
-        inter = millis();
     }
 }
